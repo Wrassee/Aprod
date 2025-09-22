173dc72b4ca9d3e9163b87d9c1b83e31e426ef5e
Wrassee <netkodok@gmail.com>
2025-09-22 23:32:31 +0200

    utolso oldal miatt completion.tsx1

--- src/pages/completion.tsx
+++ src/pages/completion.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
 import { useLanguageContext } from '@/components/language-provider';
 import { ErrorExport } from '@/components/error-export';
 import { ProtocolError } from '@shared/schema';
